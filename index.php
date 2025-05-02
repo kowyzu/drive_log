8a6e5ac7a8e9db6bc51dd135dc778f13ff9bb9ae
kowyzu <kowyzu@gmail.com>
2025-05-02 19:23:05 +0200

    feat: start new project

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>DriveLog</title>
+  <link rel="stylesheet" href="public/css/styles.css">
+
+</head>
+
+<body>
+  <div class="container p-5">
+    <div class="container text-center">
+      <div class="row">
+        <h1>Zapis svou jizdu</h1>
+      </div>
+      <div class="row">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
